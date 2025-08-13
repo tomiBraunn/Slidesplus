4e2b253324f77be90f4ecba9677afca1bdb45643
Tomas B <tomasbraun2008@gmail.com>
2025-08-13 17:30:43 -0300

    5

---
+++ frontend/componentes (por separado)/ProjectSearchBar.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+function ProjectSearchBar() {
+    return (
+        <div className="flex items-center justify-center gap-2">
+            {/* search bar */}
+            <div className="flex items-center justify-center">
+                <div className="flex items-center justify-start bg-[#1F1F1F] text-white rounded-l-full w-auto h-fit">
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        className="bg-[#1F1F1F] text-white px-4 py-2 rounded-l-full focus:outline-none w-120"
+                    />
+                    <span className="material-symbols-outlined pr-2 select-none">
+                        search
+                    </span>
+                </div>
+                <div className="flex items-center justify-center bg-[#1F1F1F] text-white rounded-r-full w-10 h-10 ml-2">
+                    <span className="material-symbols-outlined cursor-pointer select-none">
+                        add
+                    </span>
+                </div>
+            </div>
+
+            {/* view options */}
+            <div className="flex items-center justify-center bg-[#1F1F1F] text-white rounded-full gap-1 py-2 px-3 h-fit">
+                <span className="material-symbols-outlined cursor-pointer select-none">
+                    view_comfy_alt
+                </span>
+                <span className="w-[1px] h-7 bg-[#999999]"></span>
+                <span className="material-symbols-outlined cursor-pointer select-none">
+                    dehaze
+                </span>
+            </div>
+
+            {/* sort by */}
+            <div className="flex items-center justify-center bg-[#1F1F1F] text-white rounded-full gap-1 py-2 px-3 h-fit cursor-pointer select-none">
+                <p>Sort by:</p>
+                <span className="material-symbols-outlined">
+                    arrow_drop_up
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default ProjectSearchBar;
--- frontend/componentes (por separado)/ProjectTile.tsx
+++ frontend/componentes (por separado)/ProjectTile.tsx
@@ -4,15 +4,15 @@ function ProjectTile() {
 
     return (
         <div className="flex items-center justify-center">
-    <div className="flex items-start justify-center flex-col flex gap-1 bg-[#1F1F1F] text-white px-2 rounded-lg aspect-video w-50">
-        <span className="material-symbols-outlined aspect-square">
-            crop_landscape
-        </span>
-        <div class="flex items-start justify-start flex-col gap.5">
-            <p id="projectName" className="text-xl">Title</p>
-            <p id="projectDescription" className="text-xs text-[#999999]">description</p>
-        </div>
-    </div>
+            <div className="flex items-start justify-center flex-col flex gap-1 bg-[#1F1F1F] text-white px-2 rounded-lg aspect-video w-50">
+                <span className="material-symbols-outlined aspect-square">
+                    crop_landscape
+                </span>
+                <div class="flex items-start justify-start flex-col gap.5">
+                    <p id="projectName" className="text-xl">Title</p>
+                    <p id="projectDescription" className="text-xs text-[#999999]">description</p>
+                </div>
+            </div>
         </div>
     );
 }
