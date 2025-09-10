b8a4c1db1a6e624208a8adccdabd1b1d4c703b7e
Tomi <141858115+tomiBraunn@users.noreply.github.com>
2025-09-10 10:37:49 -0300

    ERROR PAGE lista

--- frontend/slides/src/components/FullScreens/UrlNotFoundedPage.tsx
+++ frontend/slides/src/components/FullScreens/UrlNotFoundedPage.tsx
@@ -13,9 +13,19 @@ function UrlNotFoundPage() {
     };
 
     return (
-        <div className="w-screen h-screen flex flex-col gap-5 items-center justify-center bg-[#121212] text-white">
-            <p>page not founded</p>
-            <button onClick={goBackOrHome} className="text-white rounded-xl border border-[#2B2B2B] bg-[#0f0f0f] hover:bg-[#161616] transition-colors cursor-pointer flex items-center justify-center px-5 py-2">goback</button>
+        <div className="w-screen h-screen flex flex-col gap-5 items-center justify-center bg-black text-white">
+            <div className='flex select-none font-extrabold gap-5'>
+                <div className='flex flex-col items-end justify-center [&>*]:text-6xl'>
+                    <p>ERROR</p>
+                    <p>404</p>
+                </div>
+                <div className='flex flex-col items start justify-center [&>*]:text-4xl'>
+                    <p>PAGE</p>
+                    <p>NOT</p>
+                    <p>FOUNDED</p>
+                </div>
+            </div>
+            <button onClick={goBackOrHome} className="text-white rounded-xl border border-[#2B2B2B] bg-[#0f0f0f] hover:bg-[#161616] transition-colors cursor-pointer flex items-center justify-center px-5 py-2 select-none">HOME PAGE</button>
         </div>
     );
 }
