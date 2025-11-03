aca2e5834f5769f70873733f2936d6b627aac8bd
Tomas B <tomasbraun2008@gmail.com>
2025-11-02 22:58:32 -0300

    25.1

--- backend/Server.js
+++ backend/Server.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(cors({
 	origin: (origin, cb) => {
-		const allowed = ["http://localhost:5173", "https://slides-plus-backend.vercel.app"]
+		const allowed = ["http://localhost:5173", "https://slides-plus-backend.vercel.app", "https://slides-plus.vercel.app/login"]
 		if (!origin || allowed.includes(origin)) return cb(null, true)
 		cb(new Error("Not allowed by CORS"))
 	},
